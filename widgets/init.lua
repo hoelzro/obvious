076a15fcc97250e163e4f6965a0dde71ccc33035
Gregor Best <farhaven@googlemail.com>
2009-02-24 22:35:23 +0100

    obvious: import init module

---
+++ widgets/init.lua
@@ -0,0 +1,6 @@
+-------------------------------------------
+-- Author: Gregor "farhaven" Best"       --
+-- Copyright 2009 Gregor Best            --
+-------------------------------------------
+
+module("widgets")
