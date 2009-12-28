04f03a70bc0d647c4efc9b0de8fe2fc17f5fe630
Uli Schlachter <psychon@znc.in>
2009-12-28 13:52:33 +0100

    Add the possibility to set width from theme
    I got too many graph widgets in my wibox to fit them all on my netbook's screen.
    With this patch I can make them fit. :)
    
    Signed-off-by: Uli Schlachter <psychon@znc.in>
    Signed-off-by: Gregor Best <gbe@intepi.net>

--- lib/widget/graph.lua
+++ lib/widget/graph.lua
@@ -17,11 +17,16 @@ function graph(layout, scale)
     local color = theme.graph_fg_color or theme.widget_fg_color or theme.fg_normal
     local back  = theme.graph_bg_color or theme.widget_bg_color or theme.bg_normal
     local border= theme.graph_border or theme.widget_border or theme.border_normal
+    local width = theme.graph_width or theme.widget_width
 
     local widget = awful.widget.graph({ layout = layout })
     widget:set_color(color)
     widget:set_border_color(border)
     widget:set_background_color(back)
+
+    if width then
+        widget:set_width(width)
+    end
 
     if scale then
         widget:set_scale(true)
