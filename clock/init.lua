f01a713615a9baf2fde82bddbdeb29809cc287c5
Serghei Iakovlev <sadhooklay@gmail.com>
2014-10-23 23:34:30 +0300

    Turns off highlighting of today for cal command

--- clock/init.lua
+++ clock/init.lua
@@ -85,7 +85,7 @@ widget:buttons(awful.util.table.join(
     else
       naughty.notify({
         text = lib.markup.font("monospace",
-        awful.util.pread("cal"):
+        awful.util.pread("cal -h"):
         gsub("([^0-9])(" .. tonumber(os.date("%d")) .. ")([^0-9])",
         "%1<span foreground=\"#FF0000\">%2</span>%3"):gsub("\n+$", "")),
         screen = capi.mouse.screen
