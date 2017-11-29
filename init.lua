fedb56c09e8151dc43c701c20e6b92a614bd956b
Rob Hoelz <rob@hoelz.ro>
2017-11-29 17:49:25 -0600

    Warn user if they use obvious modules like old-style modules
    Eventually this will be removed, but let's give people time to
    transition

--- init.lua
+++ init.lua
@@ -25,7 +25,40 @@ obvious.keymap_switch    = require 'obvious.keymap_switch'
 obvious.weather          = require 'obvious.weather'
 obvious.music            = require 'obvious.music'
 
-_G.obvious = obvious
+local naughty = require 'naughty'
+
+local function warn(msg)
+  naughty.notify {
+    title   = 'Obvious warning',
+    text    = msg,
+    timeout = 0,
+    preset  = naughty.config.presets.warn,
+  }
+end
+
+local warning_metatable = {}
+
+function warning_metatable:__index(key)
+  local module_name = 'obvious.' .. key
+  local caller_info = debug.getinfo(2, 'nSl')
+  local source = caller_info.source
+
+  if string.sub(source, 1, 1) == '@' then
+    source = string.sub(source, 2)
+  else
+    source = '<chunk>'
+  end
+
+  local location = string.format('%s:%d %s %s', source, caller_info.currentline, caller_info.namewhat, caller_info.name)
+
+  warn(string.format("You tried to use %q as an old-style Lua module at %s; from now on you'll want to do local %s = require(%q) instead", module_name, location, key, module_name))
+  rawset(self, key, obvious[key]) -- set the value on this table so that __index (and thus the warning) isn't called for this key again
+  return obvious[key]
+end
+
+local warning_table = setmetatable({}, warning_metatable)
+
+_G.obvious = warning_table
 
 return obvious
 
