ff6aeca0ae4b911455c65153ccefa242a6b4125c
Rob Hoelz <rob@hoelz.ro>
2017-11-29 17:24:39 -0600

    Load widget modules into global variable

--- init.lua
+++ init.lua
@@ -3,24 +3,30 @@
 -- Copyright 2009 Gregor Best            --
 -------------------------------------------
 
-require("obvious.basic_mpd")
-require("obvious.battery")
-require("obvious.clock")
-require("obvious.cpu")
-require("obvious.fs_usage")
-require("obvious.io")
-require("obvious.lib")
-require("obvious.loadavg")
-require("obvious.mem")
-require("obvious.net")
-require("obvious.popup_run_prompt")
-require("obvious.umts")
-require("obvious.volume_alsa")
-require("obvious.volume_freebsd")
-require("obvious.wlan")
-require("obvious.temp_info")
-require("obvious.keymap_switch")
+local obvious = {}
 
-module("obvious")
+obvious.basic_mpd        = require 'obvious.basic_mpd'
+obvious.battery          = require 'obvious.battery'
+obvious.clock            = require 'obvious.clock'
+obvious.cpu              = require 'obvious.cpu'
+obvious.fs_usage         = require 'obvious.fs_usage'
+obvious.io               = require 'obvious.io'
+obvious.lib              = require 'obvious.lib'
+obvious.loadavg          = require 'obvious.loadavg'
+obvious.mem              = require 'obvious.mem'
+obvious.net              = require 'obvious.net'
+obvious.popup_run_prompt = require 'obvious.popup_run_prompt'
+obvious.umts             = require 'obvious.umts'
+obvious.volume_alsa      = require 'obvious.volume_alsa'
+obvious.volume_freebsd   = require 'obvious.volume_freebsd'
+obvious.wlan             = require 'obvious.wlan'
+obvious.temp_info        = require 'obvious.temp_info'
+obvious.keymap_switch    = require 'obvious.keymap_switch'
+obvious.weather          = require 'obvious.weather'
+obvious.music            = require 'obvious.music'
+
+_G.obvious = obvious
+
+return obvious
 
 -- vim:ft=lua:ts=2:sw=2:sts=2:tw=80:et
